ee2c42e9f4e7678fd22b219803ba2b963850afda
Salman Dabbakuti <dabbakutisalman@gmail.com>
2021-01-22 23:34:37 +0530

    few updates

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,96 +1,132 @@
-const Portfolio = function() {
-	function makeWords() {
-		var words = [
-			{
-				text: "Smartcontracts",
-				weight: 9
-			}, {
-				text: "Web3",
-				weight: 8
-			}, {
-				text: "Solidity",
-				weight: 9
-			}, {
-				text: "JavaScript",
-				weight: 8
-			}, {
-				text: "programming",
-				weight: 9
-			}, {
-				text: "python",
-				weight: 8
-			}, {
-				text: "Chaincode",
-				weight: 9
-			}, {
-				text: "Dapps",
-				weight: 10
-			}, {
-				text: "decentralized",
-				weight: 9
-			},	{
-			text: "Ethereum",
-			weight: 12
-			}, {
-			text: "Hyperledger Fabric",
-			weight: 11
-			},{
-			text: "Node.js",
-			weight: 13
-			},{
-			text: "AWS",
-			weight: 11
-			},{
-			text: "Cloud Computing",
-			weight: 12
-			}
-		];
-		return words;
-	}
+const Portfolio = function () {
+  function makeWords() {
+    var words = [
+      {
+        text: "Smartcontracts",
+        weight: 9
+      }, {
+        text: "Web3",
+        weight: 8
+      }, {
+        text: "Solidity",
+        weight: 9
+      }, {
+        text: "JavaScript",
+        weight: 8
+      }, {
+        text: "programming",
+        weight: 9
+      }, {
+        text: "python",
+        weight: 8
+      }, {
+        text: "Chaincode",
+        weight: 9
+      }, {
+        text: "Dapps",
+        weight: 10
+      }, {
+        text: "decentralized",
+        weight: 9
+      }, {
+        text: "Ethereum",
+        weight: 12
+      }, {
+        text: "Hyperledger Fabric",
+        weight: 11
+      }, {
+        text: "Node.js",
+        weight: 13
+      }, {
+        text: "AWS",
+        weight: 11
+      }, {
+        text: "Cloud Computing",
+        weight: 12
+      },
+      {
+        text: "GraphQL",
+        weight: 13
+      },
+      {
+        text: "Ledger Databases",
+        weight: 8
+      },
+      {
+        text: "Serverless",
+        weight: 12
+      },
+      {
+        text: "Glue",
+        weight: 11
+      },
+      {
+        text: "Amplify",
+        weight: 13
+      },
+      {
+        text: "Blockchain",
+        weight: 10
+      },
+      {
+        text: "Power BI",
+        weight: 13
+      },
+      {
+        text: "CanvasJs",
+        weight: 13
+      },
+      {
+        text: "BaaS",
+        weight: 13
+      },
+    ];
+    return words;
+  }
 
-	function makeWordCloud(words) {
-		$('.teaching-domains').jQCloud(words, {delay: 120});
-	}
+  function makeWordCloud(words) {
+    $('.teaching-domains').jQCloud(words, { delay: 120 });
+  }
 
-	function displayWordCloud() {
-		var count = 1;
-		$(window).on('scroll', function() {
-			var y_scroll_pos = window.pageYOffset;
-			var scroll_pos_test = 2700; // set to whatever you want it to be
-			var words = makeWords();
-			if (y_scroll_pos > scroll_pos_test && count <= 1) {
-				makeWordCloud(words);
-				count++;
-			}
-		});
-	}
+  function displayWordCloud() {
+    var count = 1;
+    $(window).on('scroll', function () {
+      var y_scroll_pos = window.pageYOffset;
+      var scroll_pos_test = 2700; // set to whatever you want it to be
+      var words = makeWords();
+      if (y_scroll_pos > scroll_pos_test && count <= 1) {
+        makeWordCloud(words);
+        count++;
+      }
+    });
+  }
 
-	function designForm() {
-		$("#my-modal form").addClass("my-form");
-	}
+  function designForm() {
+    $("#my-modal form").addClass("my-form");
+  }
 
-	function typeAnimation() {
-		Typed.new("#writing-text", {
-			strings: [
-				"I am Blockchain Developer.", "love everything about code.", "Fascinated to program things that work in real world and useful for people in their businesses."
-			],
-			// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
-			stringsElement: null,
-			// typing speed
-			typeSpeed: 3,
-			contentType: 'text',
-			callback: function() {
-				$("#writing-text").css({"color": "#fff", "background-color": "#C8412B"});
-			},
-			preStringTyped: function() {},
-			onStringTyped: function() {}
-		});
-	}
+  function typeAnimation() {
+    Typed.new("#writing-text", {
+      strings: [
+        "I am Blockchain Developer.", "love everything about code.", "Fascinated to program things that work in real world and useful for people in their businesses."
+      ],
+      // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+      stringsElement: null,
+      // typing speed
+      typeSpeed: 3,
+      contentType: 'text',
+      callback: function () {
+        $("#writing-text").css({ "color": "#fff", "background-color": "#C8412B" });
+      },
+      preStringTyped: function () { },
+      onStringTyped: function () { }
+    });
+  }
 
-	return {
-		displayWordCloud: displayWordCloud,
-		typeAnimation: typeAnimation
-	}
+  return {
+    displayWordCloud: displayWordCloud,
+    typeAnimation: typeAnimation
+  }
 
 }();
 
